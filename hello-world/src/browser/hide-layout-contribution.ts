ef8347dd1a446b9a689c7bf10159576b6c9e29bf
pratham-devappsys <pratham@devappsys.com>
2026-02-27 16:16:41 +0530

    full working version

--- hello-world/src/browser/hide-layout-contribution.ts
+++ hello-world/src/browser/hide-layout-contribution.ts
@@ -1,8 +1,12 @@
 import { injectable, inject } from 'inversify';
 import {
     FrontendApplicationContribution,
-    ApplicationShell
+    ApplicationShell,
+        FrontendApplication
 } from '@theia/core/lib/browser';
+
+import { OutlineViewWidget } from '@theia/outline-view/lib/browser/outline-view-widget';
+
 
 @injectable()
 export class HideLayoutContribution implements FrontendApplicationContribution {
@@ -10,12 +14,21 @@ export class HideLayoutContribution implements FrontendApplicationContribution {
     @inject(ApplicationShell)
     protected readonly shell!: ApplicationShell;
 
-    async onStart(): Promise<void> {
+    async onStart(app:FrontendApplication): Promise<void> {
         await this.shell.collapsePanel('left');
         await this.shell.collapsePanel('right');
         await this.shell.collapsePanel('bottom');
 
+  const outline = app.shell.getWidgets('left')
+            .find(widget => widget instanceof OutlineViewWidget);
 
+            console.log("\n \n \n above removing the outline \n", outline);
+
+        if (outline) {
+            app.shell.closeWidget(outline.id);
+
+            console.log("\n \n \n removing the outline \n", outline.id)
+        }
        
     }
 }
--- hello-world/src/browser/open-file-contribution.ts
+++ hello-world/src/browser/open-file-contribution.ts
@@ -1,44 +1,44 @@
-import { injectable, inject } from 'inversify';
-import {
-    FrontendApplicationContribution,
-    ApplicationShell
-} from '@theia/core/lib/browser';
-import { EditorManager } from '@theia/editor/lib/browser';
-import URI from '@theia/core/lib/common/uri';
+    import { injectable, inject } from 'inversify';
+    import {
+        FrontendApplicationContribution,
+        ApplicationShell
+    } from '@theia/core/lib/browser';
+    import { EditorManager } from '@theia/editor/lib/browser';
+    import {URI} from '@theia/core/lib/common/uri';
 
-@injectable()
-export class OpenSingleFileContribution implements FrontendApplicationContribution {
+    @injectable()
+    export class OpenSingleFileContribution implements FrontendApplicationContribution {
 
-    @inject(EditorManager)
-    protected readonly editorManager!: EditorManager;
+        @inject(EditorManager)
+        protected readonly editorManager!: EditorManager;
 
-    @inject(ApplicationShell)
-    protected readonly shell!: ApplicationShell;
+        @inject(ApplicationShell)
+        protected readonly shell!: ApplicationShell;
 
-    async onDidInitializeLayout(): Promise<void> {
+        async onDidInitializeLayout(): Promise<void> {
 
-        //localStorage.setItem('openfile', 'file:///home/devappsys/Documents/projects/python-theia/main.c');
+            // localStorage.setItem('openfile', 'file:///home/devappsys/Documents/projects/python-theia/main.c');  //for testing 
 
 
-const storagePath = localStorage.getItem('openfile');
+    const storagePath = localStorage.getItem('openfile');
 
-const filePath =
-    storagePath ??
-    'file:///home/devappsys/Documents/projects/python-theia/main.c';
+    const filePath =
+        storagePath ??
+        'file:///home/devappsys/Documents/projects/python-theia/main.c';
 
-        try {
-            const uri = new URI(filePath);
+            try {
+                const uri = new URI(filePath);
 
-            const editor = await this.editorManager.open(uri, {
-                mode: 'activate'
-            });
+                const editor = await this.editorManager.open(uri, {
+                    mode: 'activate'
+                });
 
-            this.shell.activateWidget(editor.id);
+                this.shell.activateWidget(editor.id);
 
-            console.log('Opened file:', filePath);
+                console.log('Opened file:', filePath);
 
-        } catch (error) {
-            console.error('Failed to open file:', error);
+            } catch (error) {
+                console.error('Failed to open file:', error);
+            }
         }
     }
-}
